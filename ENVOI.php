bbc08916f63c6834cfa4232ee40ffc4763abd4b1
azerty211 <arbi.eldarbiev@stcharles-stecroix.org>
2026-02-03 16:51:42 +0100

    Add files via upload

---
+++ ENVOI.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Les Voitures</title>
+</head>
+<body>
+
+    <center>
+        <h1>ENvoi</h1>
+
+        <a href="BMW.html"><button>BMW</button></a>
+        <a href="MERCEDES.html"><button>MERCEDES</button></a>
+        <a href="AUDI.html"><button>AUDI</button></a>
+        <a href="PORSCHE.html"><button>PORSCHE</button></a>
+        <a href="BDD.html"><button>Accueil</button></a>
+        <br>
+        <section><br>
+            C'est bien le message que vous voulez envoyer 
+            <br>
+            <br>
+            
+
+            <?php
+            $texte = $_POST["user_Poids"];
+            echo $texte;
+
+            ?>
+
+
+            <br><button>OUI</button></a>
+            <a href="FROM.html"><button>NON</button></a>
+            </section>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+    </center>
+</body>
+</html>
